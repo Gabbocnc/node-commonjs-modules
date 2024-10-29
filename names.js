619746ee7eca386e4e9f447fd6e6411b9a3476bc
Gabbocnc <cianci.gabriele@hotmail.it>
2024-10-29 13:07:34 +0100

    add function for names.js

--- names.js
+++ names.js
@@ -2,6 +2,7 @@
 Crea una funzione che accetta due parametri: firstName, lastName. La funzione dovrebbe restituire un oggetto con le proprietà firstName e lastName.
 Esporta la funzione dal file. */
 
-function getFullName(fistName, lastName){
-    return 
+function getFullName(firstName, lastName){
+    return { firstName, lastName };
 }
+module.exports = getFullName
